7193268fffabb8a4d0830db4f5d91f5797d3a4e0
Brady Goodwin <ab.goodwin@yahoo.com>
2026-06-30 01:25:01 -0400

    Testing errors and crash fixes 1.3

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
+console.log("SERVER START FILE LOADED");
 console.log("ENV CHECK:", {
   SUPABASE_URL: process.env.SUPABASE_URL,
   SUPABASE_ANON_KEY: !!process.env.SUPABASE_ANON_KEY,
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: process.env.PORT
 });
+console.log("FILE STARTED");
+console.log("ENV:", process.env.SUPABASE_URL);
 
 import express, { type Request, Response, NextFunction } from "express";
 // import { registerRoutes } from "./routes";
