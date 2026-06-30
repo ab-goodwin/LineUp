f3981c142834dc26c891dd4698c029cc2a375989
Brady Goodwin <ab.goodwin@yahoo.com>
2026-06-30 02:40:20 -0400

    Testing errors and crash fixes 1.9

--- server/static.ts
+++ server/static.ts
@@ -1,16 +1,22 @@
 import express, { type Express } from "express";
 import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import fs from "fs";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "../public");
+  const distPath = path.resolve(process.cwd(), "dist", "public");
+  const indexPath = path.resolve(distPath, "index.html");
+
+  if (!fs.existsSync(indexPath)) {
+    console.error("Static index.html not found at:", indexPath);
+  }
 
   app.use(express.static(distPath));
 
-  app.get("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+  app.use((req, res, next) => {
+    if (req.method !== "GET") {
+      return next();
+    }
+
+    res.sendFile(indexPath);
   });
 }
